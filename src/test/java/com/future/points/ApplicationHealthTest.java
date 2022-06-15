9455b4f52bc380d4856111307719c25e77ecc129
Liam <1354243171@qq.com>
2022-06-16 07:34:14 +0800

    添加 /actuator/health 接口检查的测试用例

---
+++ src/test/java/com/future/points/ApplicationHealthTest.java
@@ -0,0 +1,29 @@
+package com.future.points;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class ApplicationHealthTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void request_health_and_return_successful() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+}
