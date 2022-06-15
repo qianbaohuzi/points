2d7ed02c712eda4097480397350ba883129066ed
Liam <1354243171@qq.com>
2022-06-15 23:48:39 +0800

    添加依赖

---
+++ src/main/java/com/future/points/PointsApplication.java
@@ -0,0 +1,15 @@
+package com.future.points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liam
+ */
+@SpringBootApplication
+public class PointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointsApplication.class, args);
+    }
+}
